035329547d52ecf63af52180d9082450d0f9ac90
Gavin Thomson <gavin.r.thomson@gmail.com>
2022-09-22 19:13:38 +0100

    first defs

---
+++ src/silverman-tate.lean
@@ -0,0 +1,19 @@
+import number_theory.dioph
+import algebra.group
+import tactic
+
+open_locale classical
+
+open set
+
+#check is_poly
+
+
+
+-- namespace silverman-tate
+
+/- def is_limit (a : ℕ → ℝ) (α : ℝ) := 
+  ∀ ε : ℝ, 0 < ε → ∃ N : ℕ, ∀ n : ℕ, N ≤ n → |a n - α| < ε -/
+
+
+-- end silverman-tate
